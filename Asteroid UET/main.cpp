c245eb04f217d91262d0af6d355fa6394ff42e5e
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2022-03-16 16:44:58 +0700

    Initializing

---
+++ Asteroid UET/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<SDL.h>
+#include<SDL_image.h>
+#include<SDL_mixer.h>
+
+// Screen dimensions
+const int SCREEN_WIDTH = 480;
+const int SCREEN_HEIGHT = 640;
+
+int main(int argc, char* args[]) {
+	// window
+	SDL_Window* window = NULL;
+
+	//
+}
