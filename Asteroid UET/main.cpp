b6ae136b35e249daf44baab6316fab21d5c2e5ff
NguyenVanHung24 <vanhung240403@gmail.com>
2022-03-17 17:34:57 +0700

    begin

--- Asteroid UET/main.cpp
+++ Asteroid UET/main.cpp
@@ -1,136 +1,42 @@
-#include<iostream>
-#include<SDL.h>
-#include<SDL_image.h>
-#include<SDL_mixer.h>
-#include<string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
 
+#include "image.h"
 
-using namespace std;
-
-
-// Screen dimensions
-const int SCREEN_WIDTH = 512;
-const int SCREEN_HEIGHT = 512;
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//Loads individual image
-SDL_Surface* loadSurface(std::string path);
-
-SDL_Window* window = NULL;
-SDL_Surface* screenSurface = NULL; // surface contained by window
-SDL_Surface* gPNGSurface = NULL; // surface of PNG image
-
-
-
-bool init() {
-	bool success = true;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cout << "SDL cannot initialize. Error: " << SDL_GetError() << endl;
-		success = false;
-	}
-	else {
-		window = SDL_CreateWindow("Asteroid Bullet", 20, 20, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL) cout << "Window could not be created. Error: " << SDL_GetError() << endl;
-		else {
-			int imgFlags = IMG_INIT_PNG;
-			if (!(IMG_Init(imgFlags) & imgFlags)) {
-				cout << "Could not initialize image. Error: " << SDL_GetError() << endl;
-				success = false;
-			}
-			else {
-				screenSurface = SDL_GetWindowSurface(window);
-			}
-		}
-	}
-	return success;
-}
-
-bool loadMedia()
+int main(int argc, char* args[])
 {
-	bool success = true;
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
 
-	gPNGSurface = loadSurface("background.png");
-	if (gPNGSurface == NULL)
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("D:/Asteroid UET/image/background.png");
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning)
 	{
-		cout << "Could not load PNG" << endl;
-		success = false;
-	}
-
-	return success;
-}
-
-void close() {
-	//Free loaded image
-	SDL_FreeSurface(gPNGSurface);
-	gPNGSurface = NULL;
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-	window = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
-
-SDL_Surface* loadSurface(string path)
-{
-	//The final optimized image
-	SDL_Surface* optimizedSurface = NULL;
-
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL)
-	{
-		cout << "Cannot load image. Error: " << SDL_GetError() << endl;
-	}
-	else
-	{
-		//Convert surface to screen format
-		optimizedSurface = SDL_ConvertSurface(loadedSurface, screenSurface->format, 0);
-		if (optimizedSurface == NULL)
+		// Get our controls and events
+		while (SDL_PollEvent(&event))
 		{
-			cout << "Unable to optimize image! SDL Error : " << SDL_GetError() << endl;
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
 		}
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface(loadedSurface);
+		window.clear();
+		window.render(grassTexture);
+		window.display();
+
 	}
 
-	return optimizedSurface;
-}
+	window.cleanUp();
+	SDL_Quit();
 
-int main(int argc, char* args[]) {
-	if (!init()) {
-		cout << "Could not initialize.";
-	}
-	else {
-		if (!loadMedia()) {
-			cout << "Could not load media.";
-		}
-		else {
-			bool quit = false;
-			SDL_Event e;
-			while (!quit) {
-				while (SDL_PollEvent(&e) != 0) {
-					if (e.type == SDL_QUIT) {
-						quit = true;
-					}
-				}
-				SDL_BlitSurface(gPNGSurface, NULL, screenSurface, NULL);
-				SDL_UpdateWindowSurface(window);
-			}
-		}
-	}
-	close();
 	return 0;
 }
